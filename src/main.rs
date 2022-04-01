2ddf05022ba19a5a5049f7efe0ad8bb796e62b5c
Caio Rezende <caiocezar50@outlook.com>
2022-03-31 22:32:41 -0300

    Initialization: Device creation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use vulkano::{
-    device::physical::PhysicalDevice,
+    device::{physical::PhysicalDevice, Device, DeviceCreateInfo, QueueCreateInfo},
     instance::{Instance, InstanceCreateInfo},
 };
 
@@ -16,4 +16,34 @@ fn main() {
             device.properties().device_name
         );
     }
+
+    let physical = PhysicalDevice::enumerate(&instance)
+        .next()
+        .expect("Nenhum dispositivo disponível");
+    // Queues são como threads de uma CPU.
+    for family in physical.queue_families() {
+        println!(
+            "Foi encontrada um 'queue family' com {:?} 'queue(s)'",
+            family.queues_count()
+        );
+    }
+
+    let queue_family = physical
+        .queue_families()
+        .find(|&q| q.supports_graphics())
+        .expect("Não foi possível encontrar uma 'queue family' grafica");
+
+    // Ao criar um dispositivo é retornado o proprio dispositivo e a lista de
+    // de filas para submissão de operações
+    let (device, mut queues) = Device::new(
+        physical,
+        DeviceCreateInfo {
+            // Passando a familia que será utilizada
+            queue_create_infos: vec![QueueCreateInfo::family(queue_family)],
+            ..Default::default()
+        },
+    )
+    .expect("Falha ao criar dispositivo");
+
+    let queue = queues.next().unwrap();
 }
